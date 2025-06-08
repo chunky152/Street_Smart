291d49fa17c3761e77dab527b21314daa0911788
chunky152 <alvinmuwanguzi1@gmail.com>
2025-06-08 19:26:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+                .clickable { },
+            text = "Hello $name!",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = "How are you?"
+        )
+
+    }
+}
+
+@Composable
+fun NewGreeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+                .clickable { },
+            text = "Hello $name!",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = "How are you?"
+        )
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_7_PRO
+)
+
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting(stringResource(R.string.alvin))
+    }
+}
+
+@Preview(
+    showBackground = true,
+)
+
+@Composable
+fun NewGreetingPreview() {
+    MyApplicationTheme {
+        NewGreeting(stringResource(R.string.alvin))
+    }
+}
